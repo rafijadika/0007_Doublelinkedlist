be73ddb0ff83ac6ac68864b6b057c8680eeceacb
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-05-04 21:55:39 +0700

    commit 2

--- 0007_Doublelinkedlist/0007_Doublelinkedlist.cpp
+++ 0007_Doublelinkedlist/0007_Doublelinkedlist.cpp
@@ -8,3 +8,29 @@ struct Node
 	Node* next;
 	Node* prev;
 };
+
+Node* START = NULL;
+
+void addnode()
+{
+	Node* newNode = new Node();
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs;
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name;
+
+	if (START == NULL || newNode->noMhs == START->noMhs)
+	{
+		if (START != NULL && newNode->noMhs == START->noMhs)
+		{
+			cout << "\033[31mDubplicate roll number not allowed\033[0m" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL)
+		{
+			START->prev = newNode;
+		}
+		newNode->prev = NULL;
+		START = newNode;
+	}
